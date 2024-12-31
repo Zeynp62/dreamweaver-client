9ffd96269b8133ca976b1628fb89fe7848ae5514
kawtharnasser <kawtherhabib.ko@gmail.com>
2024-12-31 18:39:15 +0300

    completed home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,47 @@
-import Post from './Post'
+import { useState, useEffect } from 'react'
+import CategoryFilter from './../components/Category'
 
-const Home = ({ user }) => {
+const Home = ({ user, categories }) => {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    if (categories?.length > 0) {
+      setPosts(categories[0].posts) // Default category
+    }
+  }, [categories])
+
+  const handleChange = (categorizedPosts) => {
+    setPosts(categorizedPosts)
+  };
+
   return user ? (
     <div>
       <h1>Welcome To the Home Page</h1>
-      <Post />
+
+      {/* Category component */}
+      <CategoryFilter categories={categories} onChange={handleChange} />
+
+      {/* Render filtered posts */}
+      <div>
+        <h3>Posts</h3>
+        {posts.length > 0 ? (
+          <ul>
+            {posts.map((post) => (
+              <li key={post._id}>
+                <h3>{post.title}</h3>
+                <p>{post.description}</p>
+                <img src={`http://localhost:3001/${post.postImg}`} alt={post.title} />
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>No posts available for this category.</p>
+        )}
+      </div>
     </div>
-    ) : (
-      <h3>Error: You Should Sign In to Access This Page</h3>
-    )
+  ) : (
+    <h3>Error: You Should Sign In to Access This Page</h3>
+  )
 }
 
 export default Home
